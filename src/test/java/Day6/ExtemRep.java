1f4a213088305bdc2c44d1557718f555e762ee70
rakesh <mandagirirakesh123@gmail.com>
2024-04-22 00:30:23 +0530

    changes

--- src/test/java/Day6/ExtemRep.java
+++ src/test/java/Day6/ExtemRep.java
@@ -4,7 +4,8 @@ import java.time.Duration;
 import java.util.List;
 
 
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -21,7 +22,7 @@ import org.testng.annotations.*;
 	String url;
 //	ExtentReports report;
 //	ExtentTest test;
-
+public Logger logger = LogManager.getLogger(this.getClass());
 	@BeforeMethod
 	public void setUp() throws Exception {
 //		report = new ExtentReports("C:\\Users\\MANRAKES\\Desktop\\eclipse\\Test\\long.html");
@@ -32,6 +33,8 @@ import org.testng.annotations.*;
 		url="https://www.letskodeit.com/practice";
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		logger.info("user login to website");
+
 	}
 
 	@AfterMethod
@@ -58,6 +61,7 @@ import org.testng.annotations.*;
 		s.selectByVisibleText("BMW");
 		s.selectByValue("benz");
 		s.selectByIndex(2);
+		logger.info("user selected required dropdown ");
 	}
 
 	public String captureScreen(String name) {
