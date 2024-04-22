ccc37f4e78954e2b4ee9aed202045443e2f871d2
manrakes <mandagirirakesh123@gmail.com>
2024-04-22 23:22:26 +0530

    cmt

--- src/test/java/vehicleinsuranceApp/EnterVehicleDataPage.java
+++ src/test/java/vehicleinsuranceApp/EnterVehicleDataPage.java
@@ -6,20 +6,88 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class EnterVehicleDataPage {
-    static WebDriver driver;
+     WebDriver driver;
 
-    public EnterVehicleDataPage(WebDriver driver)
+     EnterVehicleDataPage(WebDriver driver)
     {
         this.driver=driver;
 //        PageFactory.initElements(driver,this);
     }
-    public static WebElement makeVechileSel = driver.findElement(By.xpath("//select[@id='make']"));
-
+    By makeVech=By.xpath("//select[@id='make']");
+    By modelVech=By.xpath("//select[@id='model']");
+    By CylinderCap= By.xpath("//input[@id='cylindercapacity']");
+    By EnginePerf= By.xpath("//input[@id='engineperformance']");
+    By DateofManf= By.xpath("//input[@id='dateofmanufacture']");
+    By selectRigth= By.xpath("//label[normalize-space()='Yes']");
+    By NoOfSets= By.xpath("//select[@id='numberofseats']");
+    By NoOfSetsmotor= By.xpath("//select[@id='numberofseatsmotorcycle']");
+    By FuelTyoe= By.xpath("//select[@id='fuel']");
+    By payLoad= By.cssSelector("#payload");
+    By TotalWeigth= By.xpath("//input[@id='totalweight']");
+    By ListPrice= By.xpath("//input[@id='listprice']");
+    By AnnualMile= By.xpath("//input[@id='annualmileage']");
+    By nextPage= By.xpath("//button[@id='nextenterinsurantdata']");
+//     WebElement makeVechileSel = driver.findElement(By.xpath("//select[@id='make']"));
+   public void SetPayLoad(String pay)
+  {
+    driver.findElement(payLoad).sendKeys(pay);
+   }
+    public void nextPage()
+    {
+        driver.findElement(nextPage).click();
+    }
+    public void AnnualMile(String mile)
+    {
+        driver.findElement(AnnualMile).sendKeys(mile);
+    }
+    public void ListPrice(String Price)
+    {
+        driver.findElement(ListPrice).sendKeys(Price);
+    }
+   public  void SetTotalWeigth(String weigth)
+   {
+       driver.findElement(TotalWeigth).sendKeys(weigth);
+   }
+   public void getNumberOfSeats(String seats)
+   {
+    Select s= new Select(driver.findElement(NoOfSets));
+    s.selectByValue(seats);
+   }
+    public void getNumberOfSeatsMotor(String seats)
+    {
+        Select s= new Select(driver.findElement(NoOfSetsmotor));
+        s.selectByValue(seats);
+    }
+    public void getFuel(String fuel)
+    {
+        Select s= new Select(driver.findElement(FuelTyoe));
+        s.selectByValue(fuel);
+    }
     public void getMakeVechileSel(String name)
     {
-        Select s= new Select(makeVechileSel);
+        Select s= new Select(driver.findElement(makeVech));
         s.selectByVisibleText(name);
-//        return makeVechileSel;
+    }
+    public void getModelVechileSel(String Model)
+    {
+        Select s= new Select(driver.findElement(modelVech));
+        s.selectByVisibleText(Model);
+    }
+    public void setCylinderCap(String cap)
+    {
+        driver.findElement(CylinderCap).sendKeys(cap);
+    }
+    public void setEnginePer(String per )
+    {
+        driver.findElement(EnginePerf).sendKeys(per);
+    }
+    public void setDateOfManf(String date )
+    {
+        driver.findElement(DateofManf).sendKeys(date);
+    }
+    public void setRigthDrive( )
+    {
+        driver.findElement(DateofManf).click();
     }
 
 }
--- src/test/java/vehicleinsuranceApp/Insurance.java
+++ src/test/java/vehicleinsuranceApp/Insurance.java
@@ -1,27 +1,147 @@
 package vehicleinsuranceApp;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class Insurance {
-
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp() throws Exception {
+         driver = new ChromeDriver();
         //launching the Application
         driver.get("https://sampleapp.tricentis.com/101/app.php");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         driver.manage().window().maximize();
-
+    }
+    @Test
+    public void test()
+    {
         EnterVehicleDataPage vd= new EnterVehicleDataPage(driver);
-
         vd.getMakeVechileSel("Ford");
+        vd.getModelVechileSel("Three-Wheeler");
+        vd.setCylinderCap("545");
+        vd.setEnginePer("767");
+        vd.setDateOfManf("04/02/2024");
+        vd.getNumberOfSeats("1");
+        vd.setRigthDrive();
+        vd.getNumberOfSeatsMotor("1");
+        vd.getFuel("Gas");
+        vd.SetPayLoad("5");
+        vd.SetTotalWeigth("543");
+        vd.ListPrice("1000");
+        vd.AnnualMile("200");
+        vd.nextPage();
 
+        // Entering into VehicleDataPage
+        EnterInsurantDataPage Id= new EnterInsurantDataPage(driver);
+        Id.SetFirstName("Rakesh");
+        Id.SetLastName("ld");
+        Id.Setdob("02/05/2001");
+        Id.SetCountry("India");
+        Id.SetZipCode("663767");
+        Id.SetOccupation("Farmer");
+        Id.SelecHobbiesSky();
+        Id.nextPage();
 
+        //Navigate to product page
+        EnterProductDataPage pd= new EnterProductDataPage(driver);
+        pd.SetStartDate("06/05/2024");
+        pd.SetInsuranceSum("3.000.000,00");
+        pd.SetmeritData("Bonus 2");
+        pd.SetDamageIns("Partial Coverage");
+        pd.SetOptionProd();
+        pd.SetCountryCar("Yes");
+        pd.nextPage();
+
+        //Navigate to select plan
+        EnterSelectPriceOptionPage sp= new EnterSelectPriceOptionPage(driver);
+        sp.SetPlan();
+        sp.nextPage();
+
+        //Navigate to send quote page
+        EnterSendQuotePage sq= new EnterSendQuotePage(driver);
+        sq.SetEmail("raksh@gmail.com");
+        sq.SetName("rakesh");
+        sq.SetPassword("Gandi@123");
+        sq.SetConfPassword("Gandi@123");
+        sq.send();
+        Assert.assertEquals(sq.Getmsg(),"Sending e-mail success!");
+        System.out.println(sq.Getmsg());
+        sq.ClickOK();
 
     }
+    @AfterMethod
+    public void tearDown() throws Exception
+    {
+       driver.quit();
+    }
+
+//    public static void main(String[] args) {
+//        WebDriver driver = new ChromeDriver();
+//        //launching the Application
+//        driver.get("https://sampleapp.tricentis.com/101/app.php");
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+//        driver.manage().window().maximize();
+        //Navigate to Home page->VehicleDataPage
+//        EnterVehicleDataPage vd= new EnterVehicleDataPage(driver);
+//        vd.getMakeVechileSel("Ford");
+//        vd.getModelVechileSel("Three-Wheeler");
+//        vd.setCylinderCap("545");
+//        vd.setEnginePer("767");
+//        vd.setDateOfManf("04/02/2024");
+//        vd.getNumberOfSeats("1");
+//        vd.setRigthDrive();
+//        vd.getNumberOfSeatsMotor("1");
+//        vd.getFuel("Gas");
+//        vd.SetPayLoad("5");
+//        vd.SetTotalWeigth("543");
+//        vd.ListPrice("1000");
+//        vd.AnnualMile("200");
+//        vd.nextPage();
+//
+//        // Entering into VehicleDataPage
+//        EnterInsurantDataPage Id= new EnterInsurantDataPage(driver);
+//        Id.SetFirstName("Rakesh");
+//        Id.SetLastName("ld");
+//        Id.Setdob("02/05/2001");
+//        Id.SetCountry("India");
+//        Id.SetZipCode("663767");
+//        Id.SetOccupation("Farmer");
+//        Id.SelecHobbiesSky();
+//        Id.nextPage();
+//
+//        //Navigate to product page
+//        EnterProductDataPage pd= new EnterProductDataPage(driver);
+//        pd.SetStartDate("06/05/2024");
+//        pd.SetInsuranceSum("3.000.000,00");
+//        pd.SetmeritData("Bonus 2");
+//        pd.SetDamageIns("Partial Coverage");
+//        pd.SetOptionProd();
+//        pd.SetCountryCar("Yes");
+//        pd.nextPage();
+//
+//        //Navigate to select plan
+//        EnterSelectPriceOptionPage sp= new EnterSelectPriceOptionPage(driver);
+//        sp.SetPlan();
+//        sp.nextPage();
+//
+//        //Navigate to send quote page
+//        EnterSendQuotePage sq= new EnterSendQuotePage(driver);
+//        sq.SetEmail("raksh@gmail.com");
+//        sq.SetName("rakesh");
+//        sq.SetPassword("Gandi@123");
+//        sq.SetConfPassword("Gandi@123");
+//        sq.send();
+//        Assert.assertEquals(sq.Getmsg(),"Sending e-mail success!");
+//        System.out.println(sq.Getmsg());
+//        sq.ClickOK();
+
+//        driver.quit(); //close the current window
+
 }
